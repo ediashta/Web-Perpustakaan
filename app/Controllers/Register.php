fa22397d4c57e710f5cb4e2ffdd3f605b37957e0
ediashta <62034224+ediashta@users.noreply.github.com>
2020-05-03 15:55:24 +0700

    Add files via upload

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -2,11 +2,11 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Register extends BaseController
 {
     public function index()
     {
-        return view('index');
+        return view('register');
     }
     //--------------------------------------------------------------------
 
